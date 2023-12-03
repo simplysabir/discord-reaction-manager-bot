c66032a4a2c3e2a649238a503abb81c4485d55e7
simplysabir <simplysabir@gmail.com>
2023-12-03 20:25:46 +0530

    fixed command.js

--- command.js
+++ command.js
@@ -30,10 +30,7 @@ const rest = new REST({ version: "10" }).setToken(
   try {
     console.log("Started refreshing application (/) commands.");
 
-    await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-      { body: commands }
-    );
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
 
     console.log("Successfully reloaded application (/) commands.");
   } catch (error) {
--- index.js
+++ index.js
@@ -17,10 +17,47 @@ client.on("messageCreate", message => {
     if(message.author.bot) return;
     message.reply({
         content: "Hi From Bot",
-    })
-})
+    });
+});
 
-client.on("interactionCreate", interaction => {
-    console.log(interaction);
-    interaction.reply("Pong!")
-})
+client.on("interactionCreate", async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+    interaction.reply("Pong!");
+  } else if (commandName === 'clean-reactions') {
+    if (!interaction.member.permissions.has('ADMINISTRATOR')) {
+      return interaction.reply('You do not have permission to use this command.');
+    }
+
+    const channel = interaction.options.getChannel('channel');
+    const messages = await channel.messages.fetch({ limit: 100 });
+
+    for (const message of messages.values()) {
+      for (const reaction of message.reactions.cache.values()) {
+        await reaction.users.fetch();
+        for (const user of reaction.users.cache.values()) {
+          if (!user.bot) {
+            const member = await reaction.message.guild.members.fetch(user.id);
+            if (member.roles.cache.some(role => role.name === 'YourRoleName')) {
+              await reaction.users.remove(user.id);
+            }
+          }
+        }
+      }
+    }
+    interaction.reply(`Reactions cleaned in ${channel.name}`);
+  }
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+  if (reaction.message.channel.name === 'general') {
+    const member = await reaction.message.guild.members.fetch(user.id);
+    if (member.roles.cache.some(role => role.name === 'User')) {
+      await reaction.users.remove(user.id);
+    }
+  }
+});
+
