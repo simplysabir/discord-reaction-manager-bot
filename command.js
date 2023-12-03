fc578ae2c07f3cad62e0dd27744a7f9c843a3c8b
simplysabir <simplysabir@gmail.com>
2023-12-03 20:22:52 +0530

    added /clean-reactions command

--- command.js
+++ command.js
@@ -7,18 +7,36 @@ const commands = [
     name: "ping",
     description: "Replies with Pong!",
   },
+  {
+    name: 'clean-reactions',
+    description: 'Cleans up reactions in a specific channel based on user roles',
+    defaultPermission: false, // This command is restricted to certain roles
+    options: [
+      {
+        type: 7, // Channel type
+        name: 'channel',
+        description: 'The channel to clean reactions from',
+        required: true
+      },
+    ],
+  },
 ];
 
 const rest = new REST({ version: "10" }).setToken(
   process.env.AUTHORIZATION_TOKEN
 );
 
-try {
-  console.log("Started refreshing application (/) commands.");
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
 
-  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands }
+    );
 
-  console.log("Successfully reloaded application (/) commands.");
-} catch (error) {
-  console.error(error);
-}
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
